91ba43849980713c561975f44a69a089756f6680
James Alexander <jaymealexander.12@gmail.com>
2023-01-02 21:33:14 -0600

    added cards component for api call response data to show on home page

--- src/AnimeFetch.jsx
+++ src/AnimeFetch.jsx
@@ -1,11 +1,12 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 export function AnimeFetch() {
-  const fetchData = () => {
-    const baseURL = "https://kitsu.io/api/edge/anime";
+  const [anime, setAnime] = useState();
+  const baseURL = "https://kitsu.io/api/edge/anime";
+  const fetchAnimes = () => {
     axios.get(`${baseURL}`).then((res) => console.log(res.data));
   };
   useEffect(() => {
-    fetchData();
+    fetchAnimes();
   }, []);
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Header } from "./Header";
 import { Home } from "./Home";
 import { Footer } from "./Footer";
+import { AnimeFetch } from "./AnimeFetch";
 
 function App() {
   return (
     <div>
       <Header />
       <Home />
+      <AnimeFetch />
       <Footer />
     </div>
   );
