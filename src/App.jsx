c522144cbef953e8d639b74d33eda67b6075ebb8
James Alexander <jaymealexander.12@gmail.com>
2023-01-03 19:50:33 -0600

    replaces fetch with axios to call top 10 animes and display data from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import { useState, useEffect } from "react";
+import axios from "axios";
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { Sidebar } from "./Sidebar";
 import { MainAnime } from "./MainAnime";
 
 function App() {
-  const [animeList, SetAnimeList] = useState([]);
-  const [topAnime, SetTopAnime] = useState([]);
-  const [search, SetSearch] = useState([]);
+  const [animeList, setAnimeList] = useState([]);
+  const [topAnime, setTopAnime] = useState([]);
+  const [search, setSearch] = useState([]);
 
+  const getTopAnime = () => {
+    axios.get("https://api.jikan.moe/v4/top/anime?filter=%22bypopularity%22&limit=10").then((res) => {
+      console.log(res.data.data);
+      setTopAnime(res.data.data);
+    });
+  };
+  useEffect(() => {
+    getTopAnime();
+  }, []);
+  console.log(topAnime);
   return (
     <div className="App">
       <Header />
--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-export function Sidebar() {
+export function Sidebar(props) {
+  console.log(props);
   return (
     <aside>
       <nav>
         <h3>Top Anime</h3>
-        <a href="#" target="_blank" rel="noreferrer">
-          Demon Slayer
-        </a>
+        {props.topAnime.map((anime) => (
+          <a href={anime.url} target="_blank" key={anime.mal_id} rel="noreferrer">
+            {anime.title}
+          </a>
+        ))}
       </nav>
     </aside>
   );
