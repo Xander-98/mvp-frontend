3a8074f8e122c2189142b4dd4f308921d4ffb321
James Alexander <jaymealexander.12@gmail.com>
2023-01-03 20:29:37 -0600

    adds axios get request to enable search bar feature

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,25 @@ function App() {
   const [search, setSearch] = useState([]);
 
   const getTopAnime = () => {
-    axios.get("https://api.jikan.moe/v4/top/anime?filter=%22bypopularity%22&limit=10").then((res) => {
+    axios.get("https://api.jikan.moe/v4/top/anime?filter=%22bypopularity%22&limit=20").then((res) => {
       console.log(res.data.data);
       setTopAnime(res.data.data);
     });
   };
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    getAnime(search);
+  };
+
+  const getAnime = (query) => {
+    axios.get(`https://api.jikan.moe/v4/anime?q=${query}&order_by=title&sort=asc&limit=10`).then((res) => {
+      console.log(res.data.data);
+      setAnimeList(res.data.data);
+    });
+  };
+
   useEffect(() => {
     getTopAnime();
   }, []);
@@ -25,7 +39,7 @@ function App() {
       <Header />
       <div className="content-wrap">
         <Sidebar topAnime={topAnime} />
-        <MainAnime />
+        <MainAnime handleSearch={handleSearch} search={search} animeList={animeList} />
       </div>
       <Footer />
     </div>
--- src/MainAnime.jsx
+++ src/MainAnime.jsx
@@ -1,11 +1,25 @@
-export function MainAnime() {
+import React from "react";
+// import AnimeCard from "./AnimeCard";
+
+export function MainAnime(props) {
   return (
     <main>
       <div className="main-head">
-        <form action="search_box">
-          <input type="search" placeholder="Search for an anime..." required />
+        <form className="search-box" onSubmit={props.handleSearch}>
+          <input
+            type="search"
+            placeholder="Search for an anime..."
+            required
+            value={props.search}
+            onChange={(e) => props.setSearch(e.target.value)}
+          />
         </form>
       </div>
+      {/* <div className="anime-list">
+        {props.animeList.map((anime) => (
+          <AnimeCard anime={anime} key={anime.mal_id} />
+        ))}
+      </div> */}
     </main>
   );
 }
